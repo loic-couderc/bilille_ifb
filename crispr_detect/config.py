f5470530f095a91c155ce082d19812cd949e37d5
lcouderc <loic.couderc@univ-lille1.fr>
2016-11-10 09:42:40 +0100

    Remove misleading 'APPLICATION_ROOT' due to his uselessness

--- crispr_detect/config.py
+++ crispr_detect/config.py
@@ -1,4 +1,3 @@
-APPLICATION_ROOT = '/crispr/'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess-my-cristal-secret-key' #The WTF_CSRF_ENABLED setting activates the cross-site request forgery prevention
 UPLOAD_FOLDER = '/tmp/'
