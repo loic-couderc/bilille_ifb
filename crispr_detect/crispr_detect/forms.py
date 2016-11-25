0e453b3d076d6e5bd827feed03a2c93171fda5eb
lcouderc <loic.couderc@univ-lille1.fr>
2016-11-25 14:32:27 +0100

    add help block for input fields

--- crispr_detect/crispr_detect/forms.py
+++ crispr_detect/crispr_detect/forms.py
@@ -10,14 +10,24 @@ class CrisprFinderForm(Form):
     sequence = FileField( validators=[
         FileRequired(), FileAllowed(['fasta', 'fa'], 'Fasta only (.fa/.fasta)!')
     ])
-    k_mer_size_filter = IntegerField(default=3)
-    pattern = StringField(default="####_####")
-    window_size = IntegerField(default=200)
-    allowed_mismatch = IntegerField(default=1)
-    spacer_dr_match_limit = IntegerField(default=20)
-    min_dr = IntegerField(default=23)
-    max_dr = IntegerField(default=55)
-    min_spacer_dr_ratio = FloatField(default=0.6)
-    max_spacer_dr_ratio = FloatField(default=2.5)
-    first_pass_limit = IntegerField(default=200)
-    search_tracrrna = SelectField(choices=[('False','no'),('True','yes')], default='False')
+    k_mer_size_filter = IntegerField(default=3,
+                                     description='is used to compare segments of DRs and spacers')
+    pattern = StringField(default="####_####",
+                          description='is a combination of \'#\' (character) and \'_\' (space) used as seeds during second pass. First pass used continuous seed of length k_mer_size (number of \'#\')')
+    window_size = IntegerField(default=200,
+                               description='defines size of window in which k_mers will be searched for')
+    allowed_mismatch = IntegerField(default=1,
+                                    description='defines a number of mismatch allowed in repeat')
+    spacer_dr_match_limit = IntegerField(default=20,
+                                         description='is a maximum number of matches of length k_mer_size_filter per spacer between DR and spacer')
+    min_dr = IntegerField(default=23,
+                          description='defines minimum length in bp of repeat part')
+    max_dr = IntegerField(default=55,
+                          description='defines maximum length in bp of repeat part')
+    min_spacer_dr_ratio = FloatField(default=0.6,
+                                     description='is minimum quotient allowed length of spacer versus DR')
+    max_spacer_dr_ratio = FloatField(default=2.5,
+                                     description='is maximum quotient allowed length of spacer versus DR')
+    first_pass_limit = IntegerField(default=200,
+                                    description='is maximum allowed distance between two regions with repeats')
+    search_tracrrna = SelectField(choices=[('False','no'),('True','yes')], default='False', description='')
