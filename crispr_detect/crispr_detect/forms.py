bd1e91b4e3ebadbdef42d8194a7da1148e1826db
lcouderc <loic.couderc@univ-lille1.fr>
2016-11-25 15:43:26 +0100

    use label when default string formating is no appropriate

--- crispr_detect/crispr_detect/forms.py
+++ crispr_detect/crispr_detect/forms.py
@@ -19,15 +19,21 @@ class CrisprFinderForm(Form):
     allowed_mismatch = IntegerField(default=1,
                                     description='defines a number of mismatch allowed in repeat')
     spacer_dr_match_limit = IntegerField(default=20,
-                                         description='is a maximum number of matches of length k_mer_size_filter per spacer between DR and spacer')
+                                         description='is a maximum number of matches of length k_mer_size_filter per spacer between DR and spacer',
+                                         label='Spacer DR Match Limit')
     min_dr = IntegerField(default=23,
-                          description='defines minimum length in bp of repeat part')
+                          description='defines minimum length in bp of repeat part',
+                          label='Min DR')
     max_dr = IntegerField(default=55,
-                          description='defines maximum length in bp of repeat part')
+                          description='defines maximum length in bp of repeat part',
+                          label='Max DR')
     min_spacer_dr_ratio = FloatField(default=0.6,
-                                     description='is minimum quotient allowed length of spacer versus DR')
+                                     description='is minimum quotient allowed length of spacer versus DR',
+                                     label='Min Spacer DR Ratio')
     max_spacer_dr_ratio = FloatField(default=2.5,
-                                     description='is maximum quotient allowed length of spacer versus DR')
+                                     description='is maximum quotient allowed length of spacer versus DR',
+                                     label='Max Spacer DR Ratio')
     first_pass_limit = IntegerField(default=200,
                                     description='is maximum allowed distance between two regions with repeats')
-    search_tracrrna = SelectField(choices=[('False','no'),('True','yes')], default='False', description='')
+    search_tracrrna = SelectField(choices=[('False','no'),('True','yes')], default='False', description='',
+                                  label='Search tracrRNA')
