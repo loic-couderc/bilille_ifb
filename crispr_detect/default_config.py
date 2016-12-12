c0937db7259f2b3e972bdee22809c36e5a5d996d
lcouderc <loic.couderc@univ-lille1.fr>
2016-12-12 15:01:40 +0100

    fix consistency between current_upload folder and output_mount

--- crispr_detect/default_config.py
+++ crispr_detect/default_config.py
@@ -2,7 +2,7 @@ WTF_CSRF_ENABLED = True
 # The WTF_CSRF_ENABLED setting activates the cross-site request forgery
 # prevention
 SECRET_KEY = "you-will-never-guess-my-cristal-secret-key"
-UPLOAD_FOLDER = '/tmp/'
+UPLOAD_FOLDER = '/tmp/crispr'
 DEBUG = True
 THREADED = True
 CRISPR_OUPUT_MOUNT= "/tmp/crispr" #is the dir used for --volume /tmp/crispr:/crispr_detect/upload:rw \
